cf2f276c4f778aeaffede076ca3cee60a2215038
Paul <amatokus8669@gmail.com>
2019-02-16 19:58:31 +0100

    Ajout du projet déjà existant

---
+++ Calculatrice/Calculatrice/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculatrice
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            ListeOperations = new ObservableCollection<Operation>();
+            this.DataContext = this;
+            InitializeComponent();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<Operation> _listeOperations;
+
+        public ObservableCollection<Operation> ListeOperations
+        {
+            get
+            {
+                return _listeOperations;
+            }
+            set
+            {
+                _listeOperations = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ListeOperations)));
+            }
+        }
+
+        private string _saisie;
+
+        public string Saisie
+        {
+            get { return _saisie; }
+            set { _saisie = value; }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ListeOperations.Add(new Operation(Saisie));
+        }
+    }
+}
---
+++ Calculatrice/Calculatrice/Operation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatrice
+{
+    public class Operation
+    {
+        private string _entree;
+
+        public string Entree
+        {
+            get { return _entree; }
+            set { _entree = value; }
+        }
+
+        private float _resultat;
+
+        public float Resultat
+        {
+            get { return _resultat; }
+            set { _resultat = value; }
+        }
+
+        public override string ToString()
+        {
+            return Entree + "\n" + Resultat;
+        }
+
+        public Operation(string entre)
+        {
+            Entree = entre;
+            Resultat = 0;
+        }
+    }
+}
