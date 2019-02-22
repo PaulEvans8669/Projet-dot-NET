f520f41eb99c42d5750698f3a61e5692c3725125
quintus42 <quentin.laurent@etu.univ-lyon1.fr>
2019-02-22 12:31:25 +0100

    calculatrice presque finie

---
+++ Calculatrice/Calculatrice/BaseNotifyPropertyChanged.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Template_ListBox
+{
+    public abstract class BaseNotifyPropertyChanged : INotifyPropertyChanged
+    {
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Dictionary<string, object> _values = new Dictionary<string, object>();
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected object GetField([CallerMemberName] string propertyName = null)
+        {
+            if (_values.ContainsKey(propertyName)) return _values[propertyName];
+            return null;
+        }
+        protected bool SetField<T>(T value, [CallerMemberName] string propertyName = null)
+        {
+            T field = default(T);
+
+            if (_values.ContainsKey(propertyName))
+            {
+                field = (T)_values[propertyName];
+                _values[propertyName] = value;
+            }
+            else
+            {
+                _values.Add(propertyName, value);
+            }
+
+            return SetField(ref field, value, propertyName);
+        }
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #endregion
+    }
+}
+
--- Calculatrice/Calculatrice/Operation.cs
+++ Calculatrice/Calculatrice/Operation.cs
@@ -1,28 +1,33 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Template_ListBox;
 
-namespace Calculatrice
+namespace Calculatrice 
 {
-    public class Operation
+    public class Operation : BaseNotifyPropertyChanged
     {
-        private string _entree;
-
         public string Entree
         {
-            get { return _entree; }
-            set { _entree = value; }
+            get { return (string)GetField(); }
+            set { SetField(value); }
         }
-
-        private float _resultat;
 
         public float Resultat
         {
-            get { return _resultat; }
-            set { _resultat = value; }
+            get { return (float)GetField(); }
+            set { SetField(value); }
         }
+
+        public List<string> Variable
+        {
+            get { return (List<string>)GetField(); }
+            set { SetField(value); }
+        }
+
 
         public override string ToString()
         {
@@ -32,7 +37,429 @@ namespace Calculatrice
         public Operation(string entre)
         {
             Entree = entre;
-            Resultat = 0;
+            Variable = new List<string>();
+            effectuerCalcul();
         }
+
+        public bool isANumber(char c)
+        {
+            if ('0' <= c && c <= '9')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool isAOperator(char c)
+        {
+            if (c=='*' || c == '/' || c == '+' || c == '-')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void power(ref char[] tab)
+        {
+            string varTemp1 = "";
+            string varTemp2 = "";
+            int x = 0;
+            for (int i = 0; i < tab.Length; i++)
+            {
+                if (tab[i]=='^')
+                {
+                    if (tab[i-1]==')')
+                    {
+                        int nbParenthese = 0;
+                        int k = 0;
+                        for (int j = i - 1; j >= 0; j--)
+                        {
+                            if (tab[j]==')')
+                            {
+                                nbParenthese ++;
+                            }
+                            if (tab[j]=='(' && nbParenthese == 1)
+                            {
+                                k = j;
+                                break;
+                            }
+                            if (tab[j]=='(' && nbParenthese != 1)
+                            {
+                                nbParenthese--;
+                            }
+                        }
+                        char[] temp = splitTab(tab, k,i-1);
+                        varTemp1 = evaluerParenthese(temp);
+                    }
+                    else
+                    {
+                        for (int j = i - 1; j >= 0; j--)
+                        {
+                            if (isANumber(tab[j]))
+                            {
+                                varTemp1 = string.Concat(tab[j], varTemp1);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (isANumber(tab[i+1]))
+                    {
+                        for (int j = i+1; j < tab.Length; j++)
+                        {
+                            if (isANumber(tab[j]))
+                            {
+                                varTemp2 = string.Concat(varTemp2, tab[j]);
+                                x = j;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        x = parenthese(ref tab, i);
+                        char[] temp = splitTab(tab, i, x);
+                        varTemp2 = evaluerParenthese(temp);
+                    }
+                    string value = Math.Pow(double.Parse(varTemp1), double.Parse(varTemp2)).ToString();
+                    tab = concatTab(tab, value, i-(varTemp1.Length), x);
+                }
+            }
+
+        }
+
+        private void sqrt(ref char[] tab)
+        {
+            
+            for (int i = 0; i < tab.Length; i++)
+            {
+                
+                if (tab[i]== '√')
+                {
+
+                    int x = parenthese(ref tab, i+1);
+                    char[] temp = splitTab(tab, i+1, x);
+                    string varTemp = evaluerParenthese(temp);
+                    string value = Math.Sqrt(double.Parse(varTemp)).ToString();
+                    tab = concatTab(tab, value, i, x);
+                }
+
+            }
+
+        }
+
+        private void exp(ref char[] tab)
+        {
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+
+                if (tab[i] == 'e')
+                {
+
+                    int x = parenthese(ref tab, i + 3);
+                    char[] temp = splitTab(tab, i + 3, x);
+                    string varTemp = evaluerParenthese(temp);
+                    string value = Math.Exp(double.Parse(varTemp)).ToString();
+                    tab = concatTab(tab, value, i, x);
+                }
+
+            }
+
+        }
+
+        private void sin(ref char[] tab)
+        {
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+
+                if (tab[i] == 's')
+                {
+
+                    int x = parenthese(ref tab, i + 3);
+                    char[] temp = splitTab(tab, i + 3, x);
+                    string varTemp = evaluerParenthese(temp);
+                    string value = Math.Sin(double.Parse(varTemp)).ToString();
+                    tab = concatTab(tab, value, i, x);
+                }
+
+            }
+
+        }
+
+        private void cos(ref char[] tab)
+        {
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+
+                if (tab[i] == 'c')
+                {
+
+                    int x = parenthese(ref tab, i + 3);
+                    char[] temp = splitTab(tab, i + 3, x);
+                    string varTemp = evaluerParenthese(temp);
+                    string value = Math.Cos(double.Parse(varTemp)).ToString();
+                    tab = concatTab(tab, value, i, x);
+                }
+
+            }
+
+        }
+
+        private void ln(ref char[] tab)
+        {
+
+            for (int i = 0; i < tab.Length; i++)
+            {
+
+                if (tab[i] == 'l')
+                {
+
+                    int x = parenthese(ref tab, i + 2);
+                    char[] temp = splitTab(tab, i + 2, x);
+                    string varTemp = evaluerParenthese(temp);
+                    string value = Math.Log10(double.Parse(varTemp)).ToString();
+                    tab = concatTab(tab, value, i, x);
+                }
+
+            }
+
+        }
+
+        private float calculerVariable(List<string> temp)
+        {
+            while (temp.Count > 1)
+            {
+                for (int i = 0; i < temp.Count; i++)
+                {
+                    if (temp[i] == "*" || temp[i] == "/")
+                    {
+                        float res = 0;
+                        if (temp[i] == "*")
+                        {
+                            res = float.Parse(temp[i - 1]) * float.Parse(temp[i + 1]);
+                        }
+                        if (temp[i] == "/")
+                        {
+                            res = float.Parse(temp[i - 1]) / float.Parse(temp[i + 1]);
+                        }
+                        temp[i] = res.ToString();
+                        temp.RemoveAt(i - 1);
+                        temp.RemoveAt(i);
+                        i--;
+                    }
+                }
+
+                for (int i = 0; i < temp.Count; i++)
+                {
+                    if (temp[i] == "+" || temp[i] == "-")
+                    {
+                        float res = 0;
+                        if (temp[i] == "+")
+                        {
+                            res = float.Parse(temp[i - 1]) + float.Parse(temp[i + 1]);
+                        }
+                        if (temp[i] == "-")
+                        {
+                            res = float.Parse(temp[i - 1]) - float.Parse(temp[i + 1]);
+                        }
+                        temp[i] = res.ToString();
+                        temp.RemoveAt(i - 1);
+                        temp.RemoveAt(i);
+                        i--;
+                    }
+                }
+
+            }
+
+            return float.Parse(temp[0]);
+        }
+
+        private string evaluerParenthese(char[] var)
+        {
+            char[] tab = splitTab(var, 1, var.Count()-1);
+            string varTemp = "";
+
+            power(ref tab);
+
+            sqrt(ref tab);
+
+            exp(ref tab);
+
+            List<string> temp = new List<string>();
+
+            if (tab[0]!='-')
+            {
+                for (int i = 0; i < tab.Count(); i++)
+                {
+                    if (isANumber(tab[i]) || tab[i] == ',')
+                    {
+                        varTemp = string.Concat(varTemp, tab[i].ToString());
+                    }
+                    else if (tab[i] == '\0')
+                    {
+                        temp.Add(varTemp);
+                        varTemp = "";
+                    }
+                    else if (tab[i]=='-' && (tab[i-1]=='+' || tab[i - 1] == '-' || tab[i - 1] == '*' || tab[i - 1] == '/'))
+                    {
+                        varTemp += "-";
+                    }
+                    else
+                    {
+                        temp.Add(varTemp);
+                        temp.Add(tab[i].ToString());
+                        varTemp = "";
+                    }
+                }
+            }
+            else
+            {
+                varTemp += "-";
+                for (int i = 1; i < tab.Count(); i++)
+                {
+                    if (isANumber(tab[i]) || tab[i] == ',')
+                    {
+                        varTemp = string.Concat(varTemp, tab[i].ToString());
+                    }
+                    else if (tab[i] == '\0')
+                    {
+                        temp.Add(varTemp);
+                    }
+                    else
+                    {
+                        temp.Add(varTemp);
+                        temp.Add(tab[i].ToString());
+                        varTemp = "";
+                    }
+                }
+            }
+            if (varTemp!="")
+            {
+                temp.Add(varTemp);
+            }
+            return calculerVariable(temp).ToString();
+            
+        }
+
+        private char[] splitTab(char[] tab, int debut, int fin)
+        {
+            System.Console.WriteLine(debut+" || "+fin + " || " + ((fin - debut)-1));
+
+             char[] newTab = new char[fin - debut +1];
+
+            for (int i = debut; i < fin; i++)
+            {
+                newTab[i - debut] = tab[i];
+            }
+
+            return newTab;
+        }
+
+        private char[] concatTab(char[] tab, string val, int d, int f)
+        {
+
+            char[] temp1 = new char[d+1];
+            char[] temp2 = new char[tab.Count() - f];
+            for (int i = 0; i < tab.Count(); i++)
+            {
+                if (i < d)
+                {
+                    temp1[i] = tab[i];
+                }
+                else if (i > f)
+                {
+                    temp2[i-(f+1)] = tab[i];
+                }
+            }
+
+            string s = string.Concat(new string(temp1).Replace("\0",""), val, new string(temp2).Replace("\0", ""));
+
+            return s.ToArray();
+
+        }
+
+        private int parenthese(ref char[] tab, int index)
+        {
+            bool autreParenthese = false;
+            for (int i = index+1 ; i < tab.Count(); i++)
+            {
+                if (tab[i]=='(')
+                {
+                    //autreParenthese = true;
+                    int x = parenthese(ref tab, i);
+                    char[] temp = splitTab(tab, i, x);
+                    string s = evaluerParenthese(temp);
+                    tab = concatTab(tab, s, i, x);
+                }
+                else if (tab[i] == ')' && autreParenthese == false)
+                {
+                    return i;
+                }
+                else if (tab[i] == ')' && autreParenthese==true)
+                {
+                    autreParenthese = false;
+                }
+            }
+            return -1;
+        }
+
+        public void effectuerCalcul()
+        {
+
+            char[] tableau = Entree.ToArray();
+
+            power(ref tableau);
+
+            sqrt(ref tableau);
+
+            exp(ref tableau);
+
+            string varTemp = "";
+
+            for (int i = 0; i < tableau.Count(); i++)
+            {
+                if (isANumber(tableau[i]) || tableau[i]==',')
+                {
+                    varTemp = string.Concat(varTemp, tableau[i].ToString());
+                }
+                else if (isAOperator(tableau[i]))
+                {
+                    if (varTemp!="")
+                    {
+                        Variable.Add(varTemp);
+                    }
+                    Variable.Add(tableau[i].ToString());
+                    varTemp = "";
+                }
+                else if (tableau[i] == '(')
+                {
+                    int index = parenthese(ref tableau, i);
+                    char[] temp = splitTab(tableau, i, index);
+                    Variable.Add(evaluerParenthese(temp));
+                    i = index;
+                }
+            }
+            if (varTemp != "")
+            {
+                Variable.Add(varTemp);
+            }
+            Resultat = calculerVariable(Variable);
+
+        }
+        
     }
 }
